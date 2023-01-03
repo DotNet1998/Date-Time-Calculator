1e5c7d2bf4a6a1ed68d4ecc00513756ac84e09d4
mamaand007 <mamaand007@icloud.com>
2023-01-03 13:38:19 +0100

    посчитал каличество между двумя датами

--- Date Time Calculator/UserControls/Date_Comparer.cs
+++ Date Time Calculator/UserControls/Date_Comparer.cs
@@ -55,13 +55,54 @@ namespace Date_Time_Calculator.UserControls
 
             MessageBox.Show(a.ToString());
 
-            while  ((a < b && (a.AddMonths(1) < b)))
+            while  ((a < b && (a.AddMonths(1) < b))) // Месяца
             {
                 a = a.AddMonths(1);
                 m++;
             }
             lMounse.Text = m.ToString();
 
+            while ((a < b && (a.AddDays(7) < b))) // Недели
+            {
+                a = a.AddDays(7);
+                w++;
+            }
+            lWeek.Text = w.ToString();
+
+            while ((a < b && (a.AddDays(1) < b))) // Дни
+            {
+                a = a.AddDays(1);
+                d++;
+            }
+            lDay.Text = d.ToString();
+
+            while ((a < b && (a.AddHours(1) < b))) // Часы
+            {
+                a = a.AddHours(1);
+                h++;
+            }
+            lHour.Text = h.ToString();
+
+            while ((a < b && (a.AddMinutes(1) < b))) // Минуты
+            {
+                a = a.AddMinutes(1);
+                mn++;
+            }
+            lMinuts.Text = mn.ToString();
+
+            while ((a < b && (a.AddSeconds(1) < b))) // Секунды
+            {
+                a = a.AddSeconds(1);
+                s++;
+            }
+            lSecound.Text = s.ToString();
+
+            while ((a < b && (a.AddMilliseconds(1) < b))) // Милисекунды
+            {
+                a = a.AddMilliseconds(1);
+                ml++;
+            }
+            lMilisecounds.Text = ml.ToString();
 
         }
     }
