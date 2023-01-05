1da9a5985c13c0bc66e827e997fed324a61c5ca5
mamaand007 <mamaand007@icloud.com>
2023-01-05 17:19:32 +0100

    Написал код рассчитал вкладку Time_Converter

--- Date Time Calculator/UserControls/Time_Converter.Designer.cs
+++ Date Time Calculator/UserControls/Time_Converter.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
-            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.res = new Guna.UI2.WinForms.Guna2TextBox();
+            this.vvod = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
-            this.guna2ComboBox2 = new Guna.UI2.WinForms.Guna2ComboBox();
-            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.ComboBox2 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2Panel1.SuspendLayout();
@@ -59,60 +59,60 @@
             // 
             this.guna2Panel3.BorderColor = System.Drawing.Color.LightGray;
             this.guna2Panel3.BorderThickness = 1;
-            this.guna2Panel3.Controls.Add(this.guna2TextBox2);
-            this.guna2Panel3.Controls.Add(this.guna2TextBox1);
+            this.guna2Panel3.Controls.Add(this.res);
+            this.guna2Panel3.Controls.Add(this.vvod);
             this.guna2Panel3.Controls.Add(this.guna2HtmlLabel2);
             this.guna2Panel3.Controls.Add(this.guna2HtmlLabel3);
             this.guna2Panel3.Controls.Add(this.guna2Button1);
-            this.guna2Panel3.Controls.Add(this.guna2ComboBox2);
-            this.guna2Panel3.Controls.Add(this.guna2ComboBox1);
+            this.guna2Panel3.Controls.Add(this.ComboBox2);
+            this.guna2Panel3.Controls.Add(this.ComboBox1);
             this.guna2Panel3.Controls.Add(this.guna2PictureBox1);
             this.guna2Panel3.Location = new System.Drawing.Point(92, 60);
             this.guna2Panel3.Name = "guna2Panel3";
             this.guna2Panel3.Size = new System.Drawing.Size(649, 223);
             this.guna2Panel3.TabIndex = 21;
             // 
-            // guna2TextBox2
+            // res
             // 
-            this.guna2TextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox2.DefaultText = "";
-            this.guna2TextBox2.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox2.FillColor = System.Drawing.Color.WhiteSmoke;
-            this.guna2TextBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2TextBox2.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox2.Location = new System.Drawing.Point(408, 130);
-            this.guna2TextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.guna2TextBox2.Name = "guna2TextBox2";
-            this.guna2TextBox2.PasswordChar = '\0';
-            this.guna2TextBox2.PlaceholderText = "";
-            this.guna2TextBox2.SelectedText = "";
-            this.guna2TextBox2.Size = new System.Drawing.Size(109, 29);
-            this.guna2TextBox2.TabIndex = 29;
+            this.res.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.res.DefaultText = "";
+            this.res.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.res.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.res.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.res.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.res.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.res.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.res.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.res.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.res.Location = new System.Drawing.Point(408, 130);
+            this.res.Margin = new System.Windows.Forms.Padding(2);
+            this.res.Name = "res";
+            this.res.PasswordChar = '\0';
+            this.res.PlaceholderText = "";
+            this.res.SelectedText = "";
+            this.res.Size = new System.Drawing.Size(109, 29);
+            this.res.TabIndex = 29;
             // 
-            // guna2TextBox1
+            // vvod
             // 
-            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox1.DefaultText = "";
-            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.FillColor = System.Drawing.Color.WhiteSmoke;
-            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Location = new System.Drawing.Point(119, 130);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.guna2TextBox1.Name = "guna2TextBox1";
-            this.guna2TextBox1.PasswordChar = '\0';
-            this.guna2TextBox1.PlaceholderText = "";
-            this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.Size = new System.Drawing.Size(119, 29);
-            this.guna2TextBox1.TabIndex = 28;
+            this.vvod.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.vvod.DefaultText = "";
+            this.vvod.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.vvod.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.vvod.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.vvod.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.vvod.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.vvod.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.vvod.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.vvod.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.vvod.Location = new System.Drawing.Point(119, 130);
+            this.vvod.Margin = new System.Windows.Forms.Padding(2);
+            this.vvod.Name = "vvod";
+            this.vvod.PasswordChar = '\0';
+            this.vvod.PlaceholderText = "";
+            this.vvod.SelectedText = "";
+            this.vvod.Size = new System.Drawing.Size(119, 29);
+            this.vvod.TabIndex = 28;
             // 
             // guna2HtmlLabel2
             // 
@@ -153,54 +153,55 @@
             this.guna2Button1.Size = new System.Drawing.Size(76, 50);
             this.guna2Button1.TabIndex = 23;
             this.guna2Button1.Text = "Convert";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
             // 
-            // guna2ComboBox2
+            // ComboBox2
             // 
-            this.guna2ComboBox2.BackColor = System.Drawing.Color.Transparent;
-            this.guna2ComboBox2.BorderColor = System.Drawing.Color.LightGray;
-            this.guna2ComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.guna2ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.guna2ComboBox2.FillColor = System.Drawing.Color.Gainsboro;
-            this.guna2ComboBox2.FocusedColor = System.Drawing.Color.LightGray;
-            this.guna2ComboBox2.FocusedState.BorderColor = System.Drawing.Color.LightGray;
-            this.guna2ComboBox2.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.guna2ComboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
-            this.guna2ComboBox2.ItemHeight = 30;
-            this.guna2ComboBox2.Items.AddRange(new object[] {
+            this.ComboBox2.BackColor = System.Drawing.Color.Transparent;
+            this.ComboBox2.BorderColor = System.Drawing.Color.LightGray;
+            this.ComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox2.FillColor = System.Drawing.Color.Gainsboro;
+            this.ComboBox2.FocusedColor = System.Drawing.Color.LightGray;
+            this.ComboBox2.FocusedState.BorderColor = System.Drawing.Color.LightGray;
+            this.ComboBox2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.ComboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.ComboBox2.ItemHeight = 30;
+            this.ComboBox2.Items.AddRange(new object[] {
             "Лет",
             "Месяцев",
             "Дней",
             "Часов",
             "Минут",
             "Секунд"});
-            this.guna2ComboBox2.Location = new System.Drawing.Point(408, 26);
-            this.guna2ComboBox2.Name = "guna2ComboBox2";
-            this.guna2ComboBox2.Size = new System.Drawing.Size(110, 36);
-            this.guna2ComboBox2.TabIndex = 25;
+            this.ComboBox2.Location = new System.Drawing.Point(408, 26);
+            this.ComboBox2.Name = "ComboBox2";
+            this.ComboBox2.Size = new System.Drawing.Size(110, 36);
+            this.ComboBox2.TabIndex = 25;
             // 
-            // guna2ComboBox1
+            // ComboBox1
             // 
-            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2ComboBox1.BorderColor = System.Drawing.Color.LightGray;
-            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.guna2ComboBox1.FillColor = System.Drawing.Color.Gainsboro;
-            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.LightGray;
-            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.LightGray;
-            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
-            this.guna2ComboBox1.ItemHeight = 30;
-            this.guna2ComboBox1.Items.AddRange(new object[] {
+            this.ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.ComboBox1.BorderColor = System.Drawing.Color.LightGray;
+            this.ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox1.FillColor = System.Drawing.Color.Gainsboro;
+            this.ComboBox1.FocusedColor = System.Drawing.Color.LightGray;
+            this.ComboBox1.FocusedState.BorderColor = System.Drawing.Color.LightGray;
+            this.ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.ComboBox1.ItemHeight = 30;
+            this.ComboBox1.Items.AddRange(new object[] {
             "Лет",
             "Месяцев",
             "Дней",
             "Часов",
             "Минут",
             "Секунд"});
-            this.guna2ComboBox1.Location = new System.Drawing.Point(119, 26);
-            this.guna2ComboBox1.Name = "guna2ComboBox1";
-            this.guna2ComboBox1.Size = new System.Drawing.Size(115, 36);
-            this.guna2ComboBox1.TabIndex = 24;
+            this.ComboBox1.Location = new System.Drawing.Point(119, 26);
+            this.ComboBox1.Name = "ComboBox1";
+            this.ComboBox1.Size = new System.Drawing.Size(115, 36);
+            this.ComboBox1.TabIndex = 24;
             // 
             // guna2PictureBox1
             // 
@@ -248,11 +249,11 @@
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
-        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox2;
-        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Guna.UI2.WinForms.Guna2ComboBox ComboBox2;
+        private Guna.UI2.WinForms.Guna2ComboBox ComboBox1;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2TextBox res;
+        private Guna.UI2.WinForms.Guna2TextBox vvod;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
     }
 }
--- Date Time Calculator/UserControls/Time_Converter.cs
+++ Date Time Calculator/UserControls/Time_Converter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Date_Time_Calculator.UserControls
 {
@@ -15,7 +17,417 @@ namespace Date_Time_Calculator.UserControls
         public Time_Converter()
         {
             InitializeComponent();
-          
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            int y = 0;
+            int x = Convert.ToInt32(vvod.Text);
+
+
+
+            if (ComboBox1.Text == "Лет")  // ГОДА
+            {
+                if (ComboBox2.Text == "Лет")
+
+                {
+                    y = x;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Месяцев")
+
+                {
+
+                    y = x * 12;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Дней")
+
+                {
+                    y = x * 365;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Часов")
+
+                {
+                    y = x * 8760;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Минут")
+
+                {
+                    y = x * 525600;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Секунд")
+
+                {
+                    y = x * 31536000;
+                    res.Text = y.ToString();
+                }
+
+            }
+            if (ComboBox1.Text == "Месяцев")  // Месяца
+
+            {
+                if (ComboBox2.Text == "Лет")
+
+                {
+                    if (x < 12)
+                    {
+                        y =  0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 12)
+                    {
+                        y = x / 12;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Месяцев")
+
+                {
+
+                    y = x;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Дней")
+
+                {
+                    y = x * 30;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Часов")
+
+                {
+                    y = x * 730;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Минут")
+
+                {
+                    y = x * 43800;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Секунд")
+
+                {
+                    y = x * 2628003;
+                    res.Text = y.ToString();
+                }
+            }
+            if (ComboBox1.Text == "Дней")  // Дни
+
+            {
+                if (ComboBox2.Text == "Лет")
+
+                {
+                    if (x < 365)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+
+                    }
+                    if (x >= 365)
+                    {
+                        y = x / 365;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Месяцев")
+
+                {
+                    if (x < 30)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if ( x >= 30)
+                    {
+                        y = x / 30;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Дней")
+
+                {
+                    y = x;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Часов")
+
+                {
+                    y = x * 24;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Минут")
+
+                {
+                    y = x * 1440;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Секунд")
+
+                {
+                    y = x * 86400;
+                    res.Text = y.ToString();
+                }
+            }
+            if (ComboBox1.Text == "Часов")  // Часов
+
+            {
+                if (ComboBox2.Text == "Лет")
+
+                {
+                    if (x < 8760)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+
+                    }
+                    if (x >= 8760)
+                    {
+
+                        y = x / 8760;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Месяцев")
+
+                {
+                    if (x < 730)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 730)
+                    {
+                        y = x / 730;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Дней")
+
+                {
+                    if ( x < 24)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 24)
+                    {
+                        y = x / 24;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Часов")
+
+                {
+                    y = x;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Минут")
+
+                {
+                    y = x * 60;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Секунд")
+
+                {
+                    y = x * 3600;
+                    res.Text = y.ToString();
+                }
+            }
+            if (ComboBox1.Text == "Минут")  // Минут
+
+            {
+                if (ComboBox2.Text == "Лет")
+
+                {
+                    if (x < 525600)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+
+                    }
+                    if (x >= 525600)
+                    {
+
+                        y = x / 525600;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Месяцев")
+
+                {
+                    if (x < 43800)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 43800)
+                    {
+                        y = x / 43800;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Дней")
+
+                {
+                    if (x < 1440)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 1440)
+                    {
+                        y = x / 1440;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Часов")
+
+                {
+                    if ( x < 60)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 60)
+                    {
+                        y = x / 60;
+                        res.Text = y.ToString();
+                    }  
+                }
+
+                if (ComboBox2.Text == "Минут")
+
+                {
+                    y = x ;
+                    res.Text = y.ToString();
+                }
+
+                if (ComboBox2.Text == "Секунд")
+
+                {
+                    y = x * 60;
+                    res.Text = y.ToString();
+                }
+            }
+
+            if (ComboBox1.Text == "Cекунд")  // Секунд
+
+            {
+                if (ComboBox2.Text == "Лет")
+
+                {
+                    if (x < 31536000)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 31536000)
+                    {
+                        y = x / 31536000;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Месяцев")
+
+                {
+                    if (x < 2592000)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 2592000)
+                    {
+                        y = x / 2592000;
+                        res.Text = y.ToString();
+                    }
+                }
+
+                if (ComboBox2.Text == "Дней")
+
+                {
+                    if (x < 86400)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 86400)
+                    {
+                        y = x / 86400;
+                        res.Text = y.ToString();
+                    }
+
+                }
+
+                if (ComboBox2.Text == "Часов")
+
+                {
+                    if (x < 3600)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                    if (x >= 3600)
+                    {
+                        y = x / 3600;
+                        res.Text = y.ToString();
+                    }
+
+                }
+
+                if (ComboBox2.Text == "Минут")
+
+                {
+                    if (x < 60)
+                    {
+                        y = 0;
+                        res.Text = y.ToString();
+                    }
+                   if (x >= 60)
+                   {
+                        y = x / 60;
+                        res.Text = y.ToString();
+                   }
+                    
+                }
+
+                if (ComboBox2.Text == "Секунд")
+
+                {
+                    y = x ;
+                    res.Text = y.ToString();
+                }
+            }
         }
     }
 }
